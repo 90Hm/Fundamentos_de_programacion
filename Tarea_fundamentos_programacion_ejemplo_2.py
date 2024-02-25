eb527eb0f7da2a02fc0f690083aefbecacca36dd
90H <bj.camachot@uea.edu.ec>
2024-02-25 17:45:05 -0500

    Primera version.

--- Tarea_fundamentos_programacion_ejemplo_2.py
+++ Tarea_fundamentos_programacion_ejemplo_2.py
@@ -1,26 +1,25 @@
-def buscar_fruta(matriz, fruta):
-    for fila in range(len(matriz)):
-        for columna in range(len(matriz[0])):
-            if matriz[fila][columna] == fruta:
-                return True, fila, columna
-    return False, None, None
-
-
-# Definir una matriz bidimensional (3x3) con nombres de frutas
-matriz_frutas = [
+# En este segundo ejercicio utilizaremos una matriz bidimensional (3x3) con nombres de frutas de mi preferencia
+Asignacion_de_frutas = [
     ['manzana', 'pera', 'uva'],
     ['durazno', 'kiwi', 'mango'],
     ['piña', 'fresa', 'sandía']
 ]
+def buscar_fruta(Asignacion_de_frutas, fruta):
+    for fila in range(len(Asignacion_de_frutas)):
+        for columna in range(len(Asignacion_de_frutas[0])):
+            if Asignacion_de_frutas[fila][columna] == fruta:
+                return True, fila, columna
+    return False, None, None
 
-# Fruta a buscar
-fruta_a_buscar = 'kiwi'
 
-# Ejecutar búsqueda
-encontrada, fila, columna = buscar_fruta(matriz_frutas, fruta_a_buscar)
+#Agregamos el nombre de la fruta que deseamos buscar
+fruta_a_buscar = 'sandía'
 
-# Mostrar el resultado de la búsqueda
+# Este punto es importante ya que hara que se ejecute la busqueda
+encontrada, fila, columna = buscar_fruta(Asignacion_de_frutas, fruta_a_buscar)
+
+# Este apartado mostrara el resultado de la búsqueda de la fruta asignada a buscar
 if encontrada:
-    print(f"La fruta '{fruta_a_buscar}' se encontró en la posición [{fila}][{columna}] de la matriz.")
+    print(f"La fruta mencionada '{fruta_a_buscar}' se encontró en la posición de la tabla [{fila}][{columna}] de la matriz.")
 else:
-    print(f"La fruta '{fruta_a_buscar}' no se encontró en la matriz.")
+    print(f"La fruta mencionada'{fruta_a_buscar}' no se encontra dentro de la matriz.")
